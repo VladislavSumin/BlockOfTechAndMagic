4a686ad3e39841a29e53f8e2a9e19989c62e9713
Vladislav Sumin <vladislav.sumin@kaspersky.com>
2020-09-19 18:10:14 +0300

    refactoring shader exceptions

---
+++ src/main/kotlin/ru/vladislav/sumin/blockoftechandmagic/shader/ShaderExceptions.kt
@@ -0,0 +1,10 @@
+package ru.vladislav.sumin.blockoftechandmagic.shader
+
+import java.lang.Exception
+
+open class ShaderException(message: String) : Exception(message)
+
+class ShaderCompileException(message: String) : ShaderException(message)
+class ShaderAlreadyClosedException(message: String) : ShaderException(message)
+class ShaderProgramCreateException(message: String) : ShaderException(message)
+class ProgramAlreadyClosedException(message: String) : ShaderException(message)
