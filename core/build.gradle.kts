759c225841cf4d18aa5d03a3b3a24fdbfd0c150a
Vladislav Sumin <vladislav.sumin@kaspersky.com>
2020-09-23 21:06:51 +0300

    add core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,15 @@
+import ru.vladislavsumin.build.Configuration
+
+plugins {
+    java
+    kotlin("jvm")
+    kotlin("kapt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
-include(":opengl")
+include(":core", ":opengl")
 rootProject.name = "BlockOfTechAndMagic"
 
