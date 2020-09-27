388578277372237cce642398c9b7f5a5af93ae96
Vladislav Sumin <vladislav.sumin@kaspersky.com>
2020-09-27 20:48:04 +0300

    add distribution creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,25 @@ plugins {
     java
     kotlin("jvm") version "1.4.10"
     kotlin("kapt") version "1.4.10"
+    application
 }
 
 group = "ru.vladislavsumin"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClassName = "ru.vladislavsumin.blockoftechandmagic.StarterKt"
+}
+
+distributions {
+    main {
+        contents {
+            from("resources") {
+                into("resources")
+            }
+        }
+    }
+}
 
 allprojects {
     repositories {
